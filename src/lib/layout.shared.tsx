4840d24b43e425a30d2ff664620bb8c5ffef09c2
Marcus Hohlbein <marcus.hohlbein@haiilo.com>
2026-08-19 16:46:48 +0200

    Add Haiilo logo and update layout to include logo in navigation title

--- src/lib/layout.shared.tsx
+++ src/lib/layout.shared.tsx
@@ -1,11 +1,15 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
-import { appName, gitConfig } from './shared';
+import { gitConfig } from './shared';
+import Image from 'next/image';
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      // JSX supported
-      title: appName,
+      title:
+        <>
+          <Image src="/haiilo_logo.png" alt="Haiilo" className="h-6 w-auto" width={24} height={24} /> <span className="text-xl font-bold">Haiilo</span>
+        </>,
+      url: '/',
     },
     githubUrl: `https://github.com/${gitConfig.user}/${gitConfig.repo}`,
   };
